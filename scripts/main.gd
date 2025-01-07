7fedd7ecd3914afb93994144ffcef942fb0f8e72
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-07 04:14:13 -0800

    more closely matches rapier test

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,22 @@
 extends Node3D
 
-const Y_OFFSET = 10
-const CUBE_SIZE = 0.9
+const CUBE_AXIS_AMOUNT:int = 10
+const Y_OFFSET:float = 10.0
+const CUBE_SIZE:float = 0.9
+const CUBE_BOUNCINESS:float = 0.3
+
+var bouncy_material:PhysicsMaterial
+var color_material:StandardMaterial3D
 
 func _ready():
-	spawn_cubes(10)
+	bouncy_material = create_physics_material(CUBE_BOUNCINESS)
+	color_material = create_color_material(Color.LIGHT_SKY_BLUE)
+	spawn_cubes(CUBE_AXIS_AMOUNT, bouncy_material)
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	print("fps", Engine.get_frames_per_second())
 
-func spawn_cubes(axis_amount:int):
+func spawn_cubes(axis_amount:int, physics_material:PhysicsMaterial):
 	for i in axis_amount:
 		for j in axis_amount:
 			for k in axis_amount:
@@ -23,6 +30,16 @@ func spawn_cubes(axis_amount:int):
 				cube_mesh.mesh = BoxMesh.new()
 				cube_mesh.mesh.size = cube_collider.shape.size
 				cube.add_child(cube_mesh)
-				
-				cube.global_transform.origin = Vector3(i + CUBE_SIZE/2 - axis_amount/2, j + Y_OFFSET, k)
-				
+				cube.physics_material_override = physics_material
+				cube_mesh.material_override = color_material
+				cube.global_transform.origin = Vector3(i + CUBE_SIZE/2.0 - axis_amount/2.0, j + Y_OFFSET, k)
+
+func create_physics_material(bounciness:float):
+	var mat = PhysicsMaterial.new()
+	mat.bounce = bounciness
+	return mat
+
+func create_color_material(color:Color):
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = color
+	return mat
